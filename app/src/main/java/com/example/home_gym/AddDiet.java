ed08259dd41766cd21140aba2521615cca27fc6d
SuranSandeepa <sandeepa.uththamawadu@gmail.com>
2022-09-10 01:54:29 +0530

    AddDiet/ DietList/ UpdateDiet/ DietHome

---
+++ app/src/main/java/com/example/home_gym/AddDiet.java
@@ -0,0 +1,14 @@
+package com.example.home_gym;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddDiet extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_diet);
+    }
+}
