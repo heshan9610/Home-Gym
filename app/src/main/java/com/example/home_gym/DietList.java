16f7c1383e868d549e83a047fb4f532360964271
SuranSandeepa <sandeepa.uththamawadu@gmail.com>
2022-10-09 02:15:14 +0530

    Added new layout file 'raw_diet_plan'

--- app/src/main/java/com/example/home_gym/DietList.java
+++ app/src/main/java/com/example/home_gym/DietList.java
@@ -1,8 +1,10 @@
 package com.example.home_gym;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.example.home_gym.Databse.DBHandler;
 import com.example.home_gym.Models.DietModel;
@@ -12,13 +14,30 @@ import java.util.ArrayList;
 public class DietList extends AppCompatActivity {
 
     ArrayList<DietModel>DietPlans;
+    RecyclerView recyclerView;
+    TextView TotalDiets;
+    DBHandler dbHandler;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_diet_list);
 
-        DBHandler dbHandler = new DBHandler(this);
+        TotalDiets = findViewById(R.id.DietPlanTotal);
+        recyclerView = findViewById(R.id.dietRecycleView);
+
+        dbHandler = new DBHandler(this);
 
         DietPlans = dbHandler.getAllDietPlans();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        DietPlans = dbHandler.getAllDietPlans();
+
+        //get total timetable size
+        TotalDiets.setText("Total Diet Plans : " + DietPlans.size());
     }
 }
