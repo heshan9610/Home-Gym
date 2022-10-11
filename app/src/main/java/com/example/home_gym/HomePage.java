ee2a2e2015b55ded08461a31b9e7abf6b14999f6
IT20617332 <siridana@gmail.com>
2022-10-11 11:16:25 +0530

    intents updated on HomePage and LowerBodyHome pages

--- app/src/main/java/com/example/home_gym/HomePage.java
+++ app/src/main/java/com/example/home_gym/HomePage.java
@@ -2,7 +2,9 @@ package com.example.home_gym;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class HomePage extends AppCompatActivity {
 
@@ -10,6 +12,10 @@ public class HomePage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+    }
+
+    public void displayLowerBodyHomePage(View view) {
+        startActivity(new Intent(HomePage.this, LowerBodyHome.class));
     }
 
 }
