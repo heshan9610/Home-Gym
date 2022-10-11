94a2e09bd62febc06dac7d71f162f4c8035376f7
IT20617332 <siridana@gmail.com>
2022-10-11 08:40:41 +0530

    LowerBodyHome updated with methods

--- app/src/main/java/com/example/home_gym/LowerBodyHome.java
+++ app/src/main/java/com/example/home_gym/LowerBodyHome.java
@@ -2,7 +2,9 @@ package com.example.home_gym;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class LowerBodyHome extends AppCompatActivity {
 
@@ -11,4 +13,13 @@ public class LowerBodyHome extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lower_body_home);
     }
+
+    public void displayAddLowerBodyWorkoutPage(View view) {
+        startActivity(new Intent(LowerBodyHome.this, AddLowerBodyWorkout.class));
+    }
+
+    public void displayViewLowerBodyWorkoutPage(View view) {
+        startActivity(new Intent(LowerBodyHome.this, ViewLowerBodyWorkout.class));
+    }
+
 }
