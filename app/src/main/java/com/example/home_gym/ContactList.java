15bf11731896b235b53dc6650b93ee6edc8b43eb
IT20622596 <ravbuddhima@gmail.com>
2022-10-14 14:27:05 +0530

    add new files

---
+++ app/src/main/java/com/example/home_gym/ContactList.java
@@ -0,0 +1,14 @@
+package com.example.home_gym;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ContactList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contact_list);
+    }
+}
