43467297b9cfecd3343b8838ec9e74aacb58be2b
IT20622596 <ravbuddhima@gmail.com>
2022-10-19 22:27:13 +0530

    add new files

--- app/src/main/java/com/example/home_gym/ContactList.java
+++ app/src/main/java/com/example/home_gym/ContactList.java
@@ -1,8 +1,10 @@
 package com.example.home_gym;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.example.home_gym.Databse.DBHandler;
 import com.example.home_gym.Models.ContactModel;
@@ -11,6 +13,8 @@ import java.util.ArrayList;
 
 public class ContactList extends AppCompatActivity {
 
+    TextView tvTotal;
+    RecyclerView recyclerView;
     ArrayList<ContactModel> ContactDetails;
 
     @Override
@@ -18,8 +22,17 @@ public class ContactList extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact_list);
 
+
+        tvTotal =findViewById(R.id.tvTotal);
+
         DBHandler dbHandler = new DBHandler(this);
         ContactDetails = dbHandler.getAllLContactDetails();
+        tvTotal.setText("Total Contacts :" + ContactDetails.size());
+
+        for (ContactModel con : ContactDetails)
+        {
+            System.out.println(con.getName());
+        }
 
     }
 }
